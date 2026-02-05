dc980b76c445dd72d88a9f6f55781dc95a6496aa
Syud0 <nosiwoo0421@gmail.com>
2026-02-05 17:48:21 +0900

    feature/#1 룸 생성 기능 추가 및 위젯 수정

--- Source/Indian_Bab/Public/Widget/RoomCreateWidget.h
+++ Source/Indian_Bab/Public/Widget/RoomCreateWidget.h
@@ -1,8 +1,14 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "RoomCreateWidget.generated.h"
+
+
+class UButton;
+class UTextBlock;
+class UEditableTextBox;
+class USessionSubsystem;
 
 
 UCLASS()
@@ -10,4 +16,53 @@ class INDIAN_BAB_API URoomCreateWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
+public:
+	void SetParentMenu(UUserWidget* InParentMenu);
+
+protected:
+	// 위젯 생성 시 (EventConstruct) 호출
+	virtual void NativeConstruct() override;
+
+	// [추가] 위젯이 제거될 때 호출 (이벤트 해제용)
+	virtual void NativeDestruct() override;
+
+	/**
+	 * 이 위젯이 포커스를 가지고 있을 때 키 입력을 받음 (ESC 키 처리용)
+	 */
+	virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
+
+	/**
+	 * [추가] 이 위젯에서 마우스 버튼이 눌렸을 때 호출 (포커스 유지용)
+	 * 이 위젯의 '배경'을 클릭해도 포커스를 잃지 않도록 함
+	 */
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+private:
+	UPROPERTY() 
+	TObjectPtr<USessionSubsystem> SessionSubsystem;
+
+	UPROPERTY(meta = (BindWidget)) 
+	TObjectPtr<UButton> Button_Yes;
+	UPROPERTY(meta = (BindWidget)) 
+	TObjectPtr<UButton> Button_No;
+	UPROPERTY(meta = (BindWidget)) 
+	TObjectPtr<UTextBlock> Text_Status;
+	//--- 상태 변수 ---
+	UPROPERTY() 
+	TObjectPtr<APlayerController> PlayerControllerRef;
+	UPROPERTY() 
+	TObjectPtr<UUserWidget> ParentMenu;
+
+	// 로비 레벨 이름
+	UPROPERTY(EditAnywhere) 
+	FName LobbyLevelName = FName("");
+
+	UFUNCTION() 
+	void OnYesClicked();
+	UFUNCTION() 
+	void OnNoClicked();
+
+	// [추가] 서브시스템으로부터 방 생성 결과를 받을 콜백 함수
+	UFUNCTION() 
+	void OnCreateSessionComplete(bool bWasSuccessful);
 };
