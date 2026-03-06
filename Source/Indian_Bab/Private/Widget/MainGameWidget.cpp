89ca9c1ec58194123620b092e3116bea67c493ee
singa03277 <162424587+singa03277@users.noreply.github.com>
2026-03-06 10:44:59 +0900

    feature/#7 타이머 애니메이션 추가

--- Source/Indian_Bab/Private/Widget/MainGameWidget.cpp
+++ Source/Indian_Bab/Private/Widget/MainGameWidget.cpp
@@ -1,4 +1,12 @@
 #include "Widget/MainGameWidget.h"
 #include "Widget/DeckLeftWidget.h"
+#include "Components/TextBlock.h"
 
 
+void UMainGameWidget::OperateTimer() {
+	if (Time) {
+		if (RemainingTime-- > 0) {
+			Time->SetText(FText::AsNumber(RemainingTime));
+		}
+	}
+}
--- Source/Indian_Bab/Public/Widget/MainGameWidget.h
+++ Source/Indian_Bab/Public/Widget/MainGameWidget.h
@@ -6,10 +6,22 @@
 
 class UButton;
 class UDeckLeftWidget;
+class UTextBlock;
+
 
 UCLASS()
 class INDIAN_BAB_API UMainGameWidget : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float RemainingTime = 20.f;
+	
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Time;
 
+	UFUNCTION(BlueprintCallable)
+	void OperateTimer();
+
+	
 };
